86d90a27ee260d00c2a5b88ab889a0c85657af1d
Thiago Gentil <thiagosgentil@gmail.com>
2023-08-16 17:03:12 -0300

    first commit

---
+++ src/app/ofertas.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Oferta } from './shared/oferta.model';
+import { firstValueFrom } from 'rxjs';
+
+@Injectable()
+export class OfertasService {
+  constructor(private http: HttpClient) {}
+
+  public getOfertas(): Promise<Oferta[]> {
+    const observable = this.http.get<Oferta[]>('http://localhost:3000/ofertas?destaque=true');
+    return firstValueFrom(observable);
+}
+
+
+  public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
+    const observable = this.http.get<Oferta[]>(`http://localhost:3000/ofertas?categoria=${categoria}`);
+    return firstValueFrom(observable);
+}
+
+}
