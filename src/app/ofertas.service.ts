66743ab2b5c226394dc63e0579e9c7cd5335cc93
Thiago Gentil <thiagosgentil@gmail.com>
2023-08-17 11:08:05 -0300

    evitando repetições

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -2,30 +2,27 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Oferta } from './shared/oferta.model';
 import { firstValueFrom } from 'rxjs';
+import { URL_API } from './app.api';
 
 @Injectable()
 export class OfertasService {
   constructor(private http: HttpClient) {}
 
   public getOfertas(): Promise<Oferta[]> {
-    const observable = this.http.get<Oferta[]>('http://localhost:3000/ofertas?destaque=true');
+    const observable = this.http.get<Oferta[]>(`${URL_API}?destaque=true`);
     return firstValueFrom(observable);
-}
-
+  }
 
   public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
-    const observable = this.http.get<Oferta[]>(`http://localhost:3000/ofertas?categoria=${categoria}`);
+    const observable = this.http.get<Oferta[]>(`${URL_API}?categoria=${categoria}`);
     return firstValueFrom(observable);
-}
+  }
 
-public getOfertaPorId(id: number): Promise<Oferta> {
-  const observable = this.http.get<Oferta>(`http://localhost:3000/ofertas/${id}`);
-  
-  return firstValueFrom(observable)
-    .then((result: Oferta) => {
-      return result;
-    });
-}
-
-
+  public getOfertaPorId(id: number): Promise<Oferta> {
+    const observable = this.http.get<Oferta>(`${URL_API}?id=${id}`);
+    return firstValueFrom(observable)
+      .then((result: Oferta) => {
+        return result;
+      });
+  }
 }
