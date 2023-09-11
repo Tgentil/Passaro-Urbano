e4fd64a3b6f5a28d74f25b7025a0bc8d885f515e
Thiago Gentil <thiagosgentil@gmail.com>
2023-09-11 15:39:12 -0300

    rxjs interval - observable

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Oferta } from '../shared/oferta.model';
 import { OfertasService } from '../ofertas.service';
+import { Observable, interval } from 'rxjs';
 
 @Component({
   selector: 'app-oferta',
@@ -22,7 +23,25 @@ export class OfertaComponent implements OnInit {
     this.ofertasService.getOfertaPorId(this.route.snapshot.params['id'])
     .then((result: Oferta) => { 
       this.oferta = result;
-    } )
-  }
+    });
+    // this.route.params.subscribe({
+    //   next: (parametro: any) => {
+    //     console.log(parametro);
+    //   },
+    //   error: (erro: any) => {
+    //     console.log(erro);
+    //   },
+    //   complete: () => {
+    //     console.log('Processamento foi classificado como concluído!');
+    //   },
+    // });
 
+
+
+    let tempo = interval(2000);
+
+    tempo.subscribe((intervalo: number) => {
+      console.log(intervalo);
+    });
+  }
 }
