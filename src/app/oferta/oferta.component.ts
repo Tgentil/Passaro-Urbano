3239b0265771a885e7a0c847e0f17b234c8cb8fc
Thiago Gentil <thiagosgentil@gmail.com>
2023-09-11 17:39:57 -0300

    NgOnDestroy Subscribe

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -45,7 +45,7 @@ export class OfertaComponent implements OnInit, OnDestroy {
 
     let tempo = interval(2000);
 
-    this.meuObservableTesteSubscription = tempo.subscribe((intervalo: number) => {
+    this.tempoObservableSubscription = tempo.subscribe((intervalo: number) => {
       console.log(intervalo);
     });
 
@@ -59,15 +59,18 @@ export class OfertaComponent implements OnInit, OnDestroy {
     
     //observable (observador)
     this.meuObservableTesteSubscription = meuObservableTeste.subscribe({
-      next: (resultado: number) => console.log(resultado  + Number('10')),
+      next: (resultado: number) => console.log(resultado + Number('10')),
       error: (erro: string) => console.log(erro),
       complete: () => console.log('Stream de eventos foi finalizada'),
     });
   }
 
   ngOnDestroy(): void {
-    this.meuObservableTesteSubscription.unsubscribe();
-    this.tempoObservableSubscription.unsubscribe();
+    if (this.meuObservableTesteSubscription) {
+      this.meuObservableTesteSubscription.unsubscribe();
+    }
+    if (this.tempoObservableSubscription) {
+      this.tempoObservableSubscription.unsubscribe();
+    }
   }
-
 }
