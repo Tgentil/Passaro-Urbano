70a949459416253def3bbdb8d4efffee0abff150
Thiago Gentil <thiagosgentil@gmail.com>
2024-01-24 17:39:38 -0300

    router link for searchs

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -39,4 +39,8 @@ export class TopoComponent implements OnInit {
   public pesquisa(termoDaBusca: string): void {
     this.subjectPesquisa.next(termoDaBusca);
   }
+  public limpaPesquisa(): void {
+    this.subjectPesquisa.next('');
+    this.termoDaBusca = '';
+  }
 }
